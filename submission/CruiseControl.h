54a04b64dba53654444338ca47b7e8cd179573cc
Mardee Bayron <mbay613@flexitubuntu2004-template-tdc.its.auckland.ac.nz>
2025-06-08 18:54:16 +1200

    Testing teh submission program files

---
+++ submission/CruiseControl.h
@@ -0,0 +1,10 @@
+#define Zero 0.00
+
+#define SpeedMin 30.00
+#define SpeedMax 150.00
+#define SpeedInc 2.50
+#define PedalsMin 3.00
+#define CruiseSpeedTest 36.049
+#define SpeedTest 35.94
+
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed);
